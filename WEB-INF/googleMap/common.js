d9bb18805434d62485e8446785bd2f2042ae4476
Inagaki <Inagaki@VAIO>
2015-05-31 01:00:24 +0900

    googleMap調査

---
+++ WEB-INF/googleMap/common.js
@@ -0,0 +1,11 @@
+// Mapを作成
+function createMap(opts){
+    var marker = new google.maps.Marker(opts);
+    return marker;
+}
+
+// マーカーを作成
+function createMarker(opts){
+    var marker = new google.maps.Marker(opts);
+    return marker;
+}
