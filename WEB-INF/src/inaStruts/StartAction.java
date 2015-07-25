245a9d1fbbb38df40c260086dfb2149ab1f97574
Inagaki <Inagaki@VAIO>
2015-07-25 17:27:44 +0900

    poi追加

--- WEB-INF/src/inaStruts/StartAction.java
+++ WEB-INF/src/inaStruts/StartAction.java
@@ -5,6 +5,8 @@ import inaStruts.ActionForm.StartForm;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -27,8 +29,18 @@ public class StartAction extends InaAbstractAction {
 		 /* Form オブジェクトをリクエストに格納します。 */
 		 request.setAttribute("stratForm",startForm);
 		 
+		 createExcel();
+		 
 		 /* map.findForward メソッドに success を設定します。 */
 		 return mapping.findForward("success");
+	}
+
+	private void createExcel() {
+		// TODO 自動生成されたメソッド・スタブ
+		HSSFWorkbook wb1 = new HSSFWorkbook();
+	    Workbook wb3 = new HSSFWorkbook();
+	    
+	    wb3.createCellStyle();
 	}
 
 }
